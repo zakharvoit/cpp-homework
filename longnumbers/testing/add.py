04441bea53255a7f6bd466c44090dbb16f493ba6
Zakhar Voit <zakharvoit@gmail.com>
2014-03-17 18:16:07 +0400

    Deleted old location.

--- longnumbers/testing/add.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-from sys import stdin
-
-print (sum(list(map(int, stdin.read().split()))))
-
--- longnumbers/testing/sub.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from sys import stdin
-
-def diff(s):
-    return s[0] - s[1]
-
-print (diff(list(map(int, stdin.read().split()))))
-
